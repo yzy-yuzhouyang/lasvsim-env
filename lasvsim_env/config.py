276a4b9697840f1a435ae6b28961483c53256125
Zhangxt <zhzhangxiangteng@qq.com>
2025-03-11 13:31:34 +0800

    [config] rearrange all config settings

--- lasvsim_env/config.py
+++ lasvsim_env/config.py
@@ -1,29 +1,33 @@
-from dataclasses import dataclass, field
-from pathlib import Path
-import textwrap
-from typing import Any, Dict, Optional, Tuple, Union, List
+from typing import Dict, Union, Tuple
+import os
 
-from omegaconf import OmegaConf
+token_path = os.path.dirname(__file__) + "/lasvsim.token"
+print("token_path: ", token_path)
+if os.path.exists(token_path):
+    with open(token_path, 'r') as file:
+        token_str = file.read()
+else:
+    raise FileNotFoundError("Cannot find token file.")
 
-FAKE_PATH = Path('/')
-Color = Any  # Tuple[int, int, int, int]
-PolyOption = Tuple[Color, int, float, bool]  # color, layer, width, filled
-ObsNumSur = Any  # Union[int, Dict[str, int]]
+lasvsim_config = {
+    'token': token_str,
+    'task_id': 147,
+    "server_host" : 'http://172.17.0.191:8290',
+}
 
-@dataclass(frozen=False)
-class Config:
-    """Configuration for the Lasvsim environment."""
+dt = 0.1
 
-    seed: Optional[int] = None
-
-    # ===== Env =====
-    dt: float = 0.1  # Do not change this value.
-    max_steps: int = 1000
-    penalize_collision: bool = True
-    nonimal_acc: bool = False
-
-    # ===== Obs config =====
-    obs_dict: Dict[str, int] = field(default_factory=lambda: {
+env_config_multilane = {
+    "dt": dt,
+    "max_steps": 200,
+    "action_lower_bound": (-2.5 * dt, -0.065 * dt),
+    "action_upper_bound": (2.5 * dt, 0.065 * dt),
+    "real_action_lower_bound": (-1.5, -0.065),
+    "real_action_upper_bound": (0.8, 0.065),
+    "max_speed": 12.0,
+    
+    # obs config
+    "obs_dict": {
         "ego": 5,
         "single_partner": 11,
         "single_road_point": 16,
@@ -32,62 +36,52 @@ class Config:
         "max_num_agents_observed": 10,
         "max_num_map_observed": 200,
         "navi": 0,
-    })
+    },
 
-    # model free reward config
-    punish_sur_mode: str = "sum"
-    enable_slow_reward: bool = False
-    R_step: float = 5.0
-    P_lat: float = 5.0
-    P_phi: float = 20.0
-    P_yaw: float = 10.0
-    P_front: float = 5.0
-    P_side: float = 5.0
-    P_space: float = 5.0
-    P_rear: float = 5.0
-    P_steer: float = 50.0
-    P_acc: float = 0.2
-    P_delta_steer: float = 50.0
-    P_jerk: float = 0.1
-    P_boundary: float = 0.0
-    P_done: float = 2000.0
-    P_overspeed: float = 5.0
+    #  reward config
+    "nonimal_acc": True,
+    "penalize_collision": True,
+    "punish_sur_mode": "max",
+    "enable_slow_reward": True,
+    "R_step": 12.0,
+    "P_lat": 7.5/3, # lateral penalty
+    "P_phi": 2.0/3, # phi penalty
+    "P_yaw": 1.5/3, # yaw penalty
+    "P_front": 5.0,
+    "P_side": 0.0,
+    "P_space": 5.0,
+    "P_rear": 0.0,
+    "P_steer": 0.15,
+    "P_acc": 0.2,
+    "P_delta_steer": 0.0025,
+    "P_jerk": 0.003,
+    "P_done": 200.0,
+    "P_boundary": 0,
+    "P_overspeed": 5.0,
+    "safety_lat_margin_front": 0.3,
+    "safety_long_margin_front": 0.0,
+    "safety_long_margin_side": 2.0,
+    "front_dist_thd": 50.0,
+    "space_dist_thd": 12.0,
+    "rel_v_thd": 1.0,
+    "rel_v_rear_thd": 3.0,
+    "time_dist": 0.5,
+    # ref planning
 
-    safety_lat_margin_front: float = 0.0
-    safety_long_margin_front: float = 0.0
-    safety_long_margin_side: float = 0.0
-    front_dist_thd: float = 50.0
-    space_dist_thd: float = 8.0
-    rel_v_thd: float = 1.0
-    rel_v_rear_thd: float = 0.0
-    time_dist: float = 2.0
+    # Planning config
+    "qx_act_repeat_num": 1,
+    "qx_act_seq_len": 20,
 
-    # ===== Ego veh dynamics =====
-    action_lower_bound: Tuple[float, float] = (-2.0, -0.35)
-    action_upper_bound: Tuple[float, float] = ( 1.0,  0.35)
-    real_action_lower_bound: Tuple[float, float] = (-3.0, -0.065)
-    real_action_upper_bound: Tuple[float, float] = ( 0.8,  0.065)
+    "ref_horizon": 20, #  ref_horizon should be larger than qx_act_repeat_num * qx_act_seq_len
+    "ahead_lane_length_min": 6.0,
+    "ahead_lane_length_max": 60.0,
+    "v_discount_in_junction_straight": 0.75,
+    "v_discount_in_junction_left_turn": 0.3,
+    "v_discount_in_junction_right_turn": 0.3,
+    "dec_before_junction_green": 0.8,
+    "dec_before_junction_red": 1.3,
+}
 
-    # ===== Planning =====
-    qx_act_repeat_num: int = 1 # the action repeat has not been implemented
-    qx_act_seq_len: int = 20
-    ref_horizon: int = 20
 
-    # ===== Model config =====
-    max_speed: float = 8.0 # default for urban
-    v_discount_in_junction_straight: float = 0.75
-    v_discount_in_junction_left_turn: float = 0.5
-    v_discount_in_junction_right_turn: float = 0.5
-    ahead_lane_length_min: float = 6.0
-    ahead_lane_length_max: float = 60.0
-    dec_before_junction_green: float = 0.8
-    dec_before_junction_red: float = 1.3
-    min_dist_sur_length: float = 1.8
-    min_dist_sur_width: float = 1.8
-
-    
-    @staticmethod
-    def from_partial_dict(partial_dict) -> "Config":
-        base = OmegaConf.structured(Config)
-        merged = OmegaConf.merge(base, partial_dict)
-        return OmegaConf.to_object(merged)  # type: ignore
+def get_env_config() -> Dict:
+    return env_config_multilane
--- lasvsim_env/dataclass.py
+++ lasvsim_env/dataclass.py
@@ -1,10 +1,9 @@
-import torch
 import numpy as np
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, Dict, List, Generic, Optional, Tuple, Union, ClassVar
-from gops.env.env_gen_ocp.pyth_base import (Context, ContextState, Env, State, stateType)
 from operator import attrgetter
 from shapely.geometry import Point, LineString, Polygon
+from omegaconf import OmegaConf
 
 @dataclass(eq=False)
 class EgoVehicle():
@@ -56,3 +55,87 @@ class LasVSimContext():
     ego: EgoVehicle
     ref_list: List[LineString]
     sur_list: List[SurroundingVehicle]
+
+
+@dataclass(frozen=False)
+class Config:
+    """Configuration for the Lasvsim environment."""
+
+    seed: Optional[int] = None
+
+    # ===== Env =====
+    dt: float = 0.1  # Do not change this value.
+    max_steps: int = 1000
+    penalize_collision: bool = True
+    nonimal_acc: bool = False
+
+    # ===== Obs config =====
+    obs_dict: Dict[str, int] = field(default_factory=lambda: {
+        "ego": 5,
+        "single_partner": 11,
+        "single_road_point": 16,
+        "history_length": 10,
+        "partner":1100,
+        "max_num_agents_observed": 10,
+        "max_num_map_observed": 200,
+        "navi": 0,
+    })
+
+    # model free reward config
+    punish_sur_mode: str = "sum"
+    enable_slow_reward: bool = False
+    R_step: float = 5.0
+    P_lat: float = 5.0
+    P_phi: float = 20.0
+    P_yaw: float = 10.0
+    P_front: float = 5.0
+    P_side: float = 5.0
+    P_space: float = 5.0
+    P_rear: float = 5.0
+    P_steer: float = 50.0
+    P_acc: float = 0.2
+    P_delta_steer: float = 50.0
+    P_jerk: float = 0.1
+    P_boundary: float = 0.0
+    P_done: float = 2000.0
+    P_overspeed: float = 5.0
+
+    safety_lat_margin_front: float = 0.0
+    safety_long_margin_front: float = 0.0
+    safety_long_margin_side: float = 0.0
+    front_dist_thd: float = 50.0
+    space_dist_thd: float = 8.0
+    rel_v_thd: float = 1.0
+    rel_v_rear_thd: float = 0.0
+    time_dist: float = 2.0
+
+    # ===== Ego veh dynamics =====
+    action_lower_bound: Tuple[float, float] = (-2.0, -0.35)
+    action_upper_bound: Tuple[float, float] = ( 1.0,  0.35)
+    real_action_lower_bound: Tuple[float, float] = (-3.0, -0.065)
+    real_action_upper_bound: Tuple[float, float] = ( 0.8,  0.065)
+
+    # ===== Planning =====
+    qx_act_repeat_num: int = 1 # the action repeat has not been implemented
+    qx_act_seq_len: int = 20
+    ref_horizon: int = 20
+
+    # ===== Model config =====
+    max_speed: float = 8.0 # default for urban
+    v_discount_in_junction_straight: float = 0.75
+    v_discount_in_junction_left_turn: float = 0.5
+    v_discount_in_junction_right_turn: float = 0.5
+    ahead_lane_length_min: float = 6.0
+    ahead_lane_length_max: float = 60.0
+    dec_before_junction_green: float = 0.8
+    dec_before_junction_red: float = 1.3
+    min_dist_sur_length: float = 1.8
+    min_dist_sur_width: float = 1.8
+
+    
+    @staticmethod
+    def from_partial_dict(partial_dict) -> Dict[str, Any]:
+        base = OmegaConf.structured(Config)
+        merged = OmegaConf.merge(base, partial_dict)
+        return merged
+        # return OmegaConf.to_object(merged)  # type: ignore
