5e326d9645709b0acbaedc6641bb31c6d8f0d1eb
tlt22 <taolt1999@outlook.com>
2025-03-24 19:24:45 +0800

    fix(env): enhance reset method to include reset_test_vehicle parameter and validation

--- lasvsim_openapi/simulator.py
+++ lasvsim_openapi/simulator.py
@@ -221,7 +221,7 @@ class Simulator:
             StopRes,
         )
 
-    def reset(self, reset_traffic_flow: bool = False) -> ResetRes:
+    def reset(self, reset_traffic_flow: bool = False, reset_test_vehicle: dict = None) -> ResetRes:
         """Reset simulator.
 
         Args:
@@ -233,14 +233,26 @@ class Simulator:
         Raises:
             APIError: If the request fails
         """
-        return self.http_client.post(
+        assert not (reset_traffic_flow and reset_test_vehicle==None), "reset_traffic_flow and reset_test_vehicle can't be True at the same time"
+        if reset_test_vehicle is None:
+            return self.http_client.post(
             "/openapi/cosim/v2/simulation/reset",
             {
                 "simulation_id": self.simulation_id,
-                "reset_traffic_flow": reset_traffic_flow,
+                "reset_traffic_flow": reset_traffic_flow
             },
             ResetRes,
         )
+        else:
+            return self.http_client.post(
+                "/openapi/cosim/v2/simulation/reset",
+                {
+                    "simulation_id": self.simulation_id,
+                    "reset_traffic_flow": reset_traffic_flow,
+                    "reset_test_vehicle": reset_test_vehicle,
+                },
+                ResetRes,
+            )
 
     # --------- 地图部分 ---------
     def get_current_stage(self, junction_id: str) -> GetCurrentStageRes:
